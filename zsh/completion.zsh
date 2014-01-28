00fa0ff7011052b1d18c051199f6ef0731808f22
Cody Krieger <cody@krieger.io>
2014-01-28 13:51:45 -0800

    Add <type><up/down arrow> functionality for searching through history (without having to hit Ctrl-R).

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -7,3 +7,11 @@ zstyle ':completion:*' insert-tab pending
 # highlight the selected completion (i.e. ls <tab><tab>, and then you can arrow/tab around)
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:*:*:*' menu select
+
+# type, then arrow up/down to search through history beginning with what was typed
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
