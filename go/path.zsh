f69c81084ce275b43b96e38da14679ea7db1e28d
Cody Krieger <cody@krieger.io>
2015-03-07 20:57:17 -0800

    Some GOPATH changes.

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,4 @@
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:/usr/local/opt/go/libexec/bin:$PATH
+export GOPATH=$HOME
+# $HOME/bin is already in $PATH, so this is unnecessary unless $GOPATH is changed:
+# export PATH=$GOPATH/bin:$PATH
+export PATH=/usr/local/opt/go/libexec/bin:$PATH
