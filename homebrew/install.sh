ff6d919833839140bb25376c92a9ef0865a2ddca
Cody Krieger <cody@krieger.io>
2015-03-07 20:47:09 -0800

    Overhaul Homebrew and package installation process.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,35 +8,35 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  x You should probably install Homebrew first:"
-  echo "    https://github.com/mxcl/homebrew/wiki/installation"
-  exit
+  echo "  Homebrew not installed; installing..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Random utilities
-brew install spark wget tree gpg gpg2 oath-toolkit nmap hping
+# Core utilities
+brew install ack spark wget tree macvim
 
-# Dev tools
-brew install ack hub node go mercurial cvs cvsync ansible signify-osx docker boot2docker
+# Crypto/security
+brew install gpg gpg2 oath-toolkit signify-osx md5sha1sum
 
-# Infrastructure
+# Networking
+brew install nmap hping
+
+# Languages/runtimes, build tools, dependency management
+brew install node go objective-caml opam cmake carthage
+
+# Source control
+brew install hub cvs cvsync mercurial bazaar
+
+# Syncing
 brew install syncthing
 
-# IaaS/PaaS tools
-brew install heroku-toolbelt
+# IaaS/PaaS, devops tools
+brew install heroku-toolbelt ansible docker boot2docker qemu
 brew tap linode/cli
 brew install linode-cli
 
 # Games/graphics libs and tools
-brew install assimp glm
-
-# Install `brew cask` command for installing native apps.
-# brew tap phinze/homebrew-cask
-# brew install brew-cask
-
-# function installcask() {
-#   brew cask install "${@}" 2> /dev/null
-# }
+brew install assimp glm sdl sdl2
 
 # FIXME: add download + install machinery for:
 # http://i.agilebits.com/dist/1P/mac4/1Password-4.3.zip
