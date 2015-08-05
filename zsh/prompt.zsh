42e44dcc7288cfc089698698a5287cb0f211e9d7
Cody Krieger <cody@krieger.io>
2015-08-05 07:54:08 -0700

    Bring back window/tab title setting based on current directory, since only El Capitan Terminal.app supports that on its own.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,3 +3,7 @@ autoload colors && colors
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 export PROMPT=$'%{$fg_bold[red]%}➜%{$reset_color%} %n@%m: %{$fg_bold[cyan]%}%/%{$reset_color%}\n%# '
+
+precmd() {
+  title "zsh" "%n@%m" "%55<...<%~"
+}
