f1d22080f03b823a20f14cea04a3e51ea3f5ab1e
Cody Krieger <cody@krieger.io>
2016-01-20 10:05:55 -0800

    Set GO15VENDOREXPERIMENT=1

--- go/path.zsh
+++ go/path.zsh
@@ -1,4 +1,5 @@
 export GOPATH=$HOME
+export GO15VENDOREXPERIMENT=1
 # $HOME/bin is already in $PATH, so this is unnecessary unless $GOPATH is changed:
 # export PATH=$GOPATH/bin:$PATH
 export PATH=/usr/local/opt/go/libexec/bin:$PATH
