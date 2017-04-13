e4e93aa175bebc185300119b05a713e9b4159a82
Cody Krieger <cody@krieger.io>
2017-04-12 23:27:50 -0700

    Dramatically simplify PROMPT

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,1 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-
-export PROMPT=$'%{$fg_bold[red]%}➜%{$reset_color%} %n@%m: %{$fg_bold[cyan]%}%/%{$reset_color%}\n%# '
-
-precmd() {
-  title "zsh" "%n@%m" "%55<...<%~"
-}
+export PROMPT=$'%# '
