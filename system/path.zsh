6a6b35f58dec1d2a262b5eace21ef36b3d17a774
Cody Krieger <cody@krieger.io>
2017-05-16 09:40:01 -0700

    Less stupid/convoluted default PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=$HOME/bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:$PATH
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
