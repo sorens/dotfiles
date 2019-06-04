353128ecb188518a198d577eee8a3f8dba802784
Cody Krieger <cody@krieger.io>
2019-06-04 14:35:51 -0700

    fix up/down arrow history search on some linux systems

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -19,4 +19,19 @@ fi
 # like emacs).
 bindkey -e
 
+# We enable up/down arrow history search already in zsh/completion.zsh, but on
+# some Linux systems, the side effect of setting `vim` as $EDITOR enables
+# vi-mode temporarily, which we subsequently disable with bindkey -e, but at
+# that point the damage has been done and keybindings are fucked.
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  autoload -U up-line-or-beginning-search
+  zle -N up-line-or-beginning-search
+  bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+fi
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+  autoload -U down-line-or-beginning-search
+  zle -N down-line-or-beginning-search
+  bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+fi
+
 if which rbenv > /dev/null ; then eval "$(rbenv init -)" ; fi
