ec608c28e19ba5c9d5cfb7f03a04ad1478fe92f6
sorens <steve@orens.com>
2020-11-06 20:49:58 -0800

    feature: update stock brew installs

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,23 +13,21 @@ then
 fi
 
 # Core utilities
-brew install ack wget tree tmux ripgrep
+brew install ack wget tree tmux ripgrep shellcheck
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 
-# Editors
-brew install neovim
-
 # Networking
-brew install nmap hping httpie
+brew install nmap hping httpie websocat
 
 # Languages/runtimes, build tools, dependency management
-brew install go node rbenv ruby-build cloc
+brew install go node rbenv ruby-build cloc rust python@3.8
 
 # Source control
 brew install hub cvs cvsync mercurial bazaar
 
-# Syncing
-brew install syncthing
-
 # IaaS/PaaS, devops tools
+brew tap heroku/brew
 brew install awscli heroku ansible terraform packer
+
+# gpg used for signing git commits and other encryption
+brew install gpg
