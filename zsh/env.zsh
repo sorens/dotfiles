3050885d52dd825f28eea22149471a4bf2554438
Steve Orens <steve@orens.com>
2022-09-15 21:16:44 -0700

    update and improve path

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,8 +1,17 @@
 # PATH and related things
 
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:~/bin
+export PATH=$HOME/bin
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/sbin:$PATH
+export PATH=/usr/bin:$PATH
+export PATH=/usr/sbin:$PATH
+export PATH=/bin:$PATH
+export PATH=/sbin:$PATH
+export PATH=$HOME/src/bin:$PATH
+export PATH=/usr/local/go/bin:$PATH
 export PATH=/usr/local/opt/go/libexec/bin:$PATH
-export GOPATH=$HOME
+export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin":$PATH
+export GOPATH=$HOME/src
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
@@ -11,7 +20,18 @@ export PROMPT=$'%F{blue}%~%f %#> '
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 export FZF_DEFAULT_COMMAND='rg --files --hidden'
 
-export EDITOR="~/bin/subl"
+export EDITOR="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+
+# opt out of homebrew analytics
+export HOMEBREW_NO_ANALYTICS=1
+
+# homebrew
+export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+export HOMEBREW_REPOSITORY="/opt/homebrew";
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 
 # We enable up/down arrow history search already in zsh/completion.zsh, but on
 # some Linux systems, the side effect of setting `vim` as $EDITOR enables
