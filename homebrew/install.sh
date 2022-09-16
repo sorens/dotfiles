40bcde3244816a29b833bfead79f2a3ed3b7385d
Steve Orens <steve@orens.com>
2022-09-15 21:08:18 -0700

    update tools

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -20,14 +20,13 @@ brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 brew install nmap hping httpie websocat
 
 # Languages/runtimes, build tools, dependency management
-brew install go node rbenv ruby-build cloc rust python@3.8
+brew install node rbenv ruby-build rust python@3.8
 
 # Source control
-brew install hub cvs cvsync mercurial bazaar
+brew install gh
 
 # IaaS/PaaS, devops tools
-brew tap heroku/brew
-brew install awscli heroku ansible terraform packer
+brew install awscli ansible terraform packer
 
 # gpg used for signing git commits and other encryption
 # brew install gpg
