6942b941978511c53f83d3a084d03bdb8821239b
Steve Orens <steve@orens.com>
2025-06-28 07:08:30 -0700

    additional echo messages to show progress downloading

--- os-x/fonts.sh
+++ os-x/fonts.sh
@@ -6,10 +6,16 @@ SFP="$WORK_DIR/SF-Pro.dmg"
 SFC="$WORK_DIR/SF-Compact.dmg"
 SFM="$WORK_DIR/SF-Mono.dmg"
 NY="$WORK_DIR/NY.dmg"
+echo "fonts: downloading..."
+echo "fonts: downloading source code pro @ google..."
 curl -s -o "$SCP" https://fonts.google.com/download?family=Source%20Code%20Pro
-CURL -s -o "$SFP" https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg
-CURL -s -o "$SFC" https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg
-CURL -s -o "$SFM" https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg
-CURL -s -o "$NY" https://devimages-cdn.apple.com/design/resources/download/NY.dmg
-
+echo "fonts: downloading sf-pro @ apple..."
+curl -s -o "$SFP" https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg
+echo "fonts: downloading sf-compact @ apple..."
+curl -s -o "$SFC" https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg
+echo "fonts: downloading sf-mono @ apple..."
+curl -s -o "$SFM" https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg
+echo "fonts: downloading ny @ apple..."
+curl -s -o "$NY" https://devimages-cdn.apple.com/design/resources/download/NY.dmg
+echo "fonts: download complete"
 open "$WORK_DIR"
