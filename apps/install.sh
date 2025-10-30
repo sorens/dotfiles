9b33ff0caca50b6174c8ee6f628ee0806d86c6d1
Steve Orens <steve@orens.com>
2025-10-30 07:07:06 -0700

    update apps to be installed

--- apps/install.sh
+++ apps/install.sh
@@ -8,11 +8,12 @@ DOWNLOADS=(
   "VSCode-darwin-arm64.zip|https://code.visualstudio.com/sha/download?build=stable&os=darwin"
   "Slack.dmg|https://slack.com/api/desktop.latestRelease?arch=universal&variant=dmg&redirect=true"
   "Kaleidoscope.zip|https://kaleidoscope.app/download-v3"
-  "Tower.zip|https://www.git-tower.com/apps/tower3-mac/342-26634c15/Tower-9.0-342.zip"
-  "EagleFiler.dmg|https://c-command.com/downloads/EagleFiler-1.9.8.dmg"
+  "Tower.zip|https://www.git-tower.com/apps/tower3-mac/497-3c2a101a/Tower-14.5-497.zip"
+  "EagleFiler.dmg|https://c-command.com/downloads/EagleFiler-1.9.19.dmg"
   "DaisyDisk.zip|https://daisydiskapp.com/downloads/DaisyDisk.zip"
   "googlechrome.dmg|https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
   "zed.dmg|https://zed.dev/download-success?asset=Zed-aarch64.dmg&version=0.210.4&channel=stable"
+  "Xcodes.zip|https://github.com/XcodesOrg/xcodesapp/releases/latest.zip"
 )
 
 mkdir -p "$WORK_DIR"
